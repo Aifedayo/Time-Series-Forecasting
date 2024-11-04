9be7944c1480408abb5fcaa30c00d7f13f803ba7
Aifedayo <akeemifedayolag@gmail.com>
2024-11-04 10:07:35 +0100

    how to calc ema with python

---
+++ src/notebook/calc_ema.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# EMA = (current*K) + (EMA * (1-K))
+def calculate_ema(prices, period):
+    ema = [np.mean(prices[:period])]
+    multiplier  = 2/(period + 1) #k
+
+    for price in prices[period:]:
+        new_ema = (price * multiplier) + (ema[-1] * (1-multiplier))
+        ema.append(new_ema)
+
+    return np.array(ema)
+
+prices = [10, 20, 30, 40]
+ema = calculate_ema(prices, period=3)
+print("EMA:", ema)
